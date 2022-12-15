2192ebbf75ea5d99b450964b60e1d1a72ba49769
sebahattin <sebkucuk05@gmail.com>
2022-12-15 22:48:45 +0300

    ss

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -5,20 +5,30 @@ import Actions from "../redux/actions";
 function Themes() {
   const tema = useSelector((state) => state.temaReducer);
   const dispatch = useDispatch();
+  const lololo1 = {
+    backgroundColor: "yellow",
+    height: "300px",
+    width: "100%",
+  };
+  const lololo2 = {
+    backgroundColor: "orange",
+    height: "300px",
+    width: "100%",
+  };
   return (
     <div>
       {/* <button>KOYU TEMA</button>
       <button></button> */}
       <div style={tema}>
         <button
-          onClick={() => dispatch(Actions.temaAksiyon.koyu_tema("yellow"))}
+          onClick={() => dispatch(Actions.temaAksiyon.koyu_tema(lololo1))}
           type="button"
           className="btn btn-dark"
         >
           KOYU TEMA
         </button>
         <button
-          onClick={() => dispatch(Actions.temaAksiyon.acik_tema("orange"))}
+          onClick={() => dispatch(Actions.temaAksiyon.acik_tema(lololo2))}
           type="button"
           className="btn btn-light"
         >
